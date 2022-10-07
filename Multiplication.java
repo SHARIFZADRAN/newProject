8b51adfcdddebdbea14aa3dd93174de75bbfd058
sharifzadran <112987142+SHARIFZADRAN@users.noreply.github.com>
2022-10-07 12:57:29 -0700

    first commit

---
+++ Multiplication.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		{ 
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enther Multiplication Number");
+			
+			int num=sc.nextInt();
+		
+			for (int i=1; i <=10; i++)
+			
+				System.out.println(num+" * "+i+" = "+num*i);
+			}
+		}
+	
+		}
